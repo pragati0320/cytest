84cdb3b03b7907036d8122d0fb56da08b6cf4ef8
“pragati0320” <pragati0320@gmail.com>
2024-04-01 10:59:33 +0545

    second commit

---
+++ cypress/integration/test_examples/test.js
@@ -0,0 +1,12 @@
+//cypress
+/// reference types="Cypress" />
+
+describe ('first test suite', function(){
+
+cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")    
+
+cy.get('.search-keyword').type('Cau')
+}
+
+
+)
